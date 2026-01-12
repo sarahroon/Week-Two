00a9ddf66b2f190bbaa15cfd814834d2929d076b
sarahroon <sarahrooney130500@gmail.com>
2026-01-12 11:42:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
+console.log("Hello world");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My thid blog post",
+];
+
+console.log(blogPosts);
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+
+blogPosts[3] = "My fourth blog post";
+
+console.log(blogPosts[3]);
+
+const randomList = ["important reminder", 42, ["another array"], true];
+
+// ARRAYS
+
+const favouriteFoods = [
+  "My first favourite food",
+  "My second favourite food",
+  "My third favourite food",
+];
+
+console.log(favouriteFoods);
+console.log(favouriteFoods[0]);
+
+const favouriteColours = [
+  "My first favourite colour",
+  "My second favourite colour",
+  "My third favourite colour",
+];
+
+console.log(favouriteColours);
+console.log(favouriteColours[1]);
+
+const favouriteNumbers = [
+  "My first favourite number",
+  "My second favourite number",
+  "My third favourite number",
+];
+
+console.log(favouriteNumbers);
+console.log(favouriteNumbers[2]);
+  
 
