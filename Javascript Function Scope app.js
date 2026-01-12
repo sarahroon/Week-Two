c6fb6c25d3c5ff6a579e39ce9742fbdaf7655110
sarahroon <sarahrooney130500@gmail.com>
2026-01-12 15:59:09 +0000

    Update Javascript Function Scope app.js

--- Javascript Function Scope app.js
+++ Javascript Function Scope app.js
@@ -1,1 +1,29 @@
+console.log("Hello world!");
+
+const myName = "Sarah";
+
+function sayHello() {
+  console.log("Hello " + myName + "!");
+}
+
+sayHello();
+console.log(myName);
+
+function sayHello() {
+  const myName = "Sarah";
+  console.log("Hello " + myName + "!");
+}
+
+sayHello();
+
+console.log(myName);
+
+const myGlobalVariable = 'favouritecolour'
+
+function printColour() {
+
+  let printColour = "green";
+  favouritecolour = "pink";
+  console.log(myfavouritecolour, myGlobalVariable)
+}
 
