97222b36f2d0023fe5be69270a02cc50bd46d4e4
sarahroon <sarahrooney130500@gmail.com>
2026-01-13 16:10:19 +0000

    Update Javascript Objects Challenge 01 app.js

--- Javascript Objects Challenge 01 app.js
+++ Javascript Objects Challenge 01 app.js
@@ -1,1 +1,31 @@
+console.log("JavaScript is linked!");
 
+console.log("Website Advert");
+
+const car = {
+  make: "Kia"
+  model: Sportage,
+  colour: "black",
+};
+
+console.log(car);
+
+console.table(car);
+
+const car = {
+  make: "Kia",
+  model: Sportage,
+  colour: "black",
+};
+
+console.log(car.make);
+
+console.log(car.model);
+
+console.log(car.colour);
+
+const car = {
+  make: "Kia",
+  model: Sportage,
+  colour: "black",
+};
