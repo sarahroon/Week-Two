040b8378328bfa6f460a3faa6340f8a64ebf0a69
sarahroon <sarahrooney130500@gmail.com>
2026-01-13 16:22:49 +0000

    Update Javascript Objects Challenge 02 app.js

--- Javascript Objects Challenge 02 app.js
+++ Javascript Objects Challenge 02 app.js
@@ -1,1 +1,49 @@
+console.log("JavaScript is linked!");
+
+console.log("Website advert!");
+
+const book = {
+  title: "EffectiveJava",
+  author: JoshuaBloch,
+  numberofPages: "416",
+};
+
+console.log(book);
+
+console.table(book);
+
+const book = {
+  title: "EffectiveJava",
+  author: JoshuaBloch,
+  numberofPages: "416",
+};
+
+const blogPost = {
+  title: "My first blog post about the book titled Effective Java",
+  author: {
+    name: "Sarah",
+    age: 25,
+    favouriteBook: "EffectiveJavabyJoshuaBloch",
+  }'
+  tags: ["coding", "javascript", "book"],
+  content: "This is my blog post about the book written by Joshua Bloch teaching effective java coding.",
+  };
+
+console.log(blogPost.author.name);
+
+console.log(blogPost.tags[0];
+
+console.log(blogPost.tags.length);
+
+console.log(
+  `There are ${blogPosts.tags.length} tags for post "${blogPost.title}":
+);
+for (let i = 0; i < blogPost.tags.length; i++) {
+  console.log(blogPost.tags[i];
+}
+
+console.log(`Tags for post "${blogPost.title}":`);
+for (let tag of blogPost.tags) {
+  console.log(tag);
+}
 
