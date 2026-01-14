905be4d89eec242d3e44d05462437fb4150ed8f7
sarahroon <sarahrooney130500@gmail.com>
2026-01-14 13:41:43 +0000

    Update Challenge 01 Javascript Loops app.js

--- Challenge 01 Javascript Loops app.js
+++ Challenge 01 Javascript Loops app.js
@@ -1,1 +1,15 @@
+// Challenge 01 // LOOPING OVER ARRAY // for loop - RUNS FOUR TIMES
+
+console.log("Hello world!");
+
+for (let i = 0; i < 4; i++) {
+  console.log("Iteration", i);
+}
+
+const favouriteAnimals = ["dog", "cat", "rabbit", "dolphin"];
+for (let i = 0; i < favouriteAnimals.length; i++) {
+  console.log(favouriteAnimals[i];
+}
+
+
 
