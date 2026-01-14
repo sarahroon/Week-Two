2cf9ef2e4edc51426ec7555bce4d3bf589a78f74
sarahroon <sarahrooney130500@gmail.com>
2026-01-14 11:20:56 +0000

    Update Cascading Style Sheets Grid app.js

--- Cascading Style Sheets Grid app.js
+++ Cascading Style Sheets Grid app.js
@@ -1,1 +1,14 @@
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
+body {
+  height: 100vh;
+  width: 100vh;
+  font-family: monospace;
+  color: black;
+  background-color: #00FFFF;
+  text-decoration: none;
+}
