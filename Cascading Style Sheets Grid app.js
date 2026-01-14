bf44d7d99a3f97521649fd57ccb2560a12c2569e
sarahroon <sarahrooney130500@gmail.com>
2026-01-14 11:35:54 +0000

    Update Cascading Style Sheets Grid app.js

--- Cascading Style Sheets Grid app.js
+++ Cascading Style Sheets Grid app.js
@@ -13,8 +13,30 @@ body {
 
 display: grid;
 
-grid-template-columns: 1fr 2fr 1fr;  // SET AMOUNT OF COLUMNS // 1fr 1fr 1fr IS THE FRACTION UNIT
+grid-template-columns: 1fr 2fr 1fr;    // SET AMOUNT OF COLUMNS // 1fr 2fr 1fr IS THE FRACTION UNIT
+grid-template-rows: 1fr 3fr 1fr;      // SET AMOUNT OF ROWS // 1fr 3fr 1fr IS THE FRACTION UNIT
 }
+
+header {                             // MOVING THE ELEMENT
+grid-column-start: 1;               // TELLING PROGRAM WHAT ROW HEADER SHOULD BE IN  
+grid-column-start: 2;
+grid-column-end: 3;
+}
+
+main {
+   grid-row: 2 / 3;
+   grid-column: 2 / 3 ;
+
+   background-color: orange;
+}
+
+footer {
+    grid-row: 3 / 4;
+    grid-columns: 2 / 3;
+
+    background-color: purple;
+}
+
 
 a: visited {
   color: blue;
