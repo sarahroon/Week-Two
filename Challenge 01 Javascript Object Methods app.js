12ba54f3a4136ef8ec259542aec8693f0c9b7c08
sarahroon <sarahrooney130500@gmail.com>
2026-01-14 10:51:27 +0000

    Update Challenge 01 Javascript Object Methods app.js

--- Challenge 01 Javascript Object Methods app.js
+++ Challenge 01 Javascript Object Methods app.js
@@ -1,1 +1,12 @@
+const car = {
+  make: "Toyota",
+  model: "Prius",
+  colour: "Red",
+  advert() {
+   return `For sale: ${this.colour} ${this.make} ${this.model}`;
+  }
+};
 
+console.log(car.advert());
+
+// "For sale: Red Toyota Prius"
