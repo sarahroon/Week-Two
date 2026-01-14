2b8a7338dc0c94e4536ad129a3df517215ae6667
sarahroon <sarahrooney130500@gmail.com>
2026-01-14 10:15:10 +0000

    Update 14.01.26 Javascript Callback Functions app.js

--- 14.01.26 Javascript Callback Functions app.js
+++ 14.01.26 Javascript Callback Functions app.js
@@ -2,13 +2,18 @@ console.log('Test, test')
 
 fnction runsOtherFunctions(a) {
   console.log(a)
+  greet()
 }
 
-runsOtherFunctions('Sarah')
-runsOtherFunctions(4)
-
 runsOtherFunctions(greet)
+
+runsOtherFunctions('Sarah')
+
+runsOtherFunctions(1)
 
 function greet() {
   console.log(`Hello`)
 }
+
+function byeBye() {
+  console.log(`See ya!')
