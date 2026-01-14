d5ffb45fe4ae912f305d6d14c083615ef65f577f
sarahroon <sarahrooney130500@gmail.com>
2026-01-14 10:37:29 +0000

    Update Javascript Object Methods app.js

--- Javascript Object Methods app.js
+++ Javascript Object Methods app.js
@@ -1,1 +1,42 @@
+console.log("Hello world!");
 
+// METHOD
+
+const person = {
+  name: "John",
+  age: 26,
+  favouriteColour: "blue",
+  sayHello: function () {
+    console.log("Hello!");
+  },
+};
+
+person.sayHello();
+
+// PASSING ARGUMENTS
+
+const person = {
+  name: "John",
+  age: 26,
+  favouriteColour: "blue",
+  sayHello: function () {
+    console.log("Hello!");
+  },
+};
+
+person.sayHello("Kate");
+
+// RETURNING VALUES
+
+const person = {
+  name: "John",
+  age: 26,
+  favouriteColour: "blue",
+  getAge: function () {
+    return this.age;
+  },
+};
+
+const age = person.getAge();
+
+console.log(age);
