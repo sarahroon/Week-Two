8467d9e7b45432807bade92e65ca71b7ad6825e3
sarahroon <sarahrooney130500@gmail.com>
2026-01-14 13:42:20 +0000

    Update Challenge 02 Javascript Loops app.js

--- Challenge 02 Javascript Loops app.js
+++ Challenge 02 Javascript Loops app.js
@@ -1,1 +1,8 @@
+// Challenge 02 // LOOPING OVER ARRAY // forEach loop
+
+const favouriteColours = ["pink", "purple", "green", "blue"];
+favouriteColours.forEach(function (Colour) {
+  console.log(Colour);
+});
+
 
