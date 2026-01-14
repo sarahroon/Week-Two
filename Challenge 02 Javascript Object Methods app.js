5eb7b4b744b705bf14e19c0b569361faf3ebfb9c
sarahroon <sarahrooney130500@gmail.com>
2026-01-14 11:00:56 +0000

    Update Challenge 02 Javascript Object Methods app.js

--- Challenge 02 Javascript Object Methods app.js
+++ Challenge 02 Javascript Object Methods app.js
@@ -1,1 +1,12 @@
+const book = {
+  title: "JavaScriptCookbook",
+  author: "ShelleyPowers",
+  numberofPages: "634",
+  advert() {
+   return `For sale: ${this.title} ${this.author} ${this.numberofPages}`;
+  }
+};
 
+console.log(car.advert());
+
+// "For sale: title JavaScriptCookbook author ShelleyPowers number of pages 634"
