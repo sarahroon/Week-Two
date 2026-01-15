95e96b5cb4947655cbda1ea38183ff2b96586df1
sarahroon <sarahrooney130500@gmail.com>
2026-01-15 11:46:25 +0000

    Update 15.01.26 Cascading Style Sheets Grid app.js

--- 15.01.26 Cascading Style Sheets Grid app.js
+++ 15.01.26 Cascading Style Sheets Grid app.js
@@ -1,1 +1,12 @@
+.container {
+ display: grid;
+ grid-template-columns: 1fr 1fr 1fr;
+ grid-gap: 10px;
+ grid-template-rows: 1fr 1fr;
+ grid-auto-rows: 100px;
+ grid-auto-flow: column;
+}
 
+.box {
+ background-color: pink;
+}
