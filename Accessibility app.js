f75112b216ed58baa8e26182751eb9f29f744dfc
sarahroon <sarahrooney130500@gmail.com>
2026-01-15 13:30:18 +0000

    Update Accessibility app.js

--- Accessibility app.js
+++ Accessibility app.js
@@ -1,1 +1,7 @@
+const notificationArea = document.getElementbyId('notificationArea')
 
+const notificationButton = document.getElementById('notificationButton')
+
+notificationButton.addEventListener('click', function() {
+      notificationArea.textContent = "New email!"
+})
