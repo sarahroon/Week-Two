5c09349abdda057afe353781ffd1824fe4b8059d
sarahroon <sarahrooney130500@gmail.com>
2026-01-15 14:43:45 +0000

    Update Interacting With Media Elements app.js

--- Interacting With Media Elements app.js
+++ Interacting With Media Elements app.js
@@ -1,1 +1,21 @@
+const image = document.querySelector("img");
 
+image.addEventListener("load", function (event) {
+  console.log("Image loaded!", event);
+});
+
+const audio = document.querySelector("audio");
+
+audio.addEventListener("play", function (event) {
+  console.log("Playing", event);
+});
+
+const video = document.querySelector("video");
+
+video.addEventListener("pause", function (event) {
+  console.log("Pausing", event);
+});
+
+video.addEventListener("time", function (event) {
+  console.log("Time event:", event);
+});
